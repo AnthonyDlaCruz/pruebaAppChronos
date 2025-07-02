c04dd69b73254e602c6ade95231c8308245501d8
AnthonyDlaCruz <anthonyarmando3@gmail.com>
2025-07-02 17:57:50 -0400

    Frontend: apunta al backend en render

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import { getProductionOrders } from './odoo.js'; // O como lo hayas llamado
 
-const app = express(); // ✅ Definir app aquí
+const app = express();
 app.use(cors());
 
-// ✅ Luego sí puedes usar `app.get`
 app.get('/api/productionorders', async (req, res) => {
   try {
     const orders = await getProductionOrders();
@@ -16,7 +15,9 @@ app.get('/api/productionorders', async (req, res) => {
   }
 });
 
-const PORT = 3000;
+// ✅ Puerto dinámico para que Render funcione
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en http://localhost:${PORT}`);
 });
